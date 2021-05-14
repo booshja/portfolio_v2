00749edae3b2fb4dfc79d9303f97062c3fa333f5
booshja <jacob.andes@gmail.com>
2021-05-13 23:45:09 -0700

    updated uri for heroku

--- src/app.py
+++ src/app.py
@@ -7,9 +7,11 @@ from forms import LoginForm, ContactForm, RegisterForm
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    "DATABASE_URL", "postgresql:///portfolio"
-)
+uri = os.getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = uri
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "secretkey")
 app.config["MAIL_SERVER"] = "smtp.googlemail.com"
